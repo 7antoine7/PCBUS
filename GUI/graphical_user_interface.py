6b634421c6b22cd2905ab08d8abfde00cd7bd9f1
Antoine Leroux <Antoine.Leroux@usherbrooke.ca>
2022-02-20 16:07:58 -0500

    solved merge conflict

--- GUI/graphical_user_interface.py
+++ GUI/graphical_user_interface.py
@@ -29,25 +29,6 @@ squareButtonResize = int(arrowResizeX)
 rectangleButtonResizeX = int(3/2*arrowResizeX)
 rectangleButtonResizeY = int(17/30*rectangleButtonResizeX)
 
-<<<<<<< HEAD
-jogXup_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/up_X_arrow.png').resize((arrowResizeX,arrowResizeY), Image.ANTIALIAS))
-jogXdown_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/down_X_arrow.png').resize((arrowResizeX,arrowResizeY), Image.ANTIALIAS))
-jogYup_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/up_Y_arrow.png').resize((arrowResizeY,arrowResizeX), Image.ANTIALIAS))
-jogYdown_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/down_Y_arrow.png').resize((arrowResizeY,arrowResizeX), Image.ANTIALIAS))
-jogZup_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/up_Z_arrow.png').resize((arrowResizeX,arrowResizeY), Image.ANTIALIAS))
-jogZdown_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/down_Z_arrow.png').resize((arrowResizeX,arrowResizeY), Image.ANTIALIAS))
-homeXY_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/home_button.png').resize((squareButtonResize,squareButtonResize), Image.ANTIALIAS))
-mesh_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/mesh_button.png').resize((squareButtonResize,squareButtonResize), Image.ANTIALIAS))
-modeAuto_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/mode_auto.png').resize((rectangleButtonResizeX,rectangleButtonResizeY), Image.ANTIALIAS)) 
-modeMan_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/mode_manuel.png').resize((rectangleButtonResizeX,rectangleButtonResizeY), Image.ANTIALIAS)) 
-modeSingle_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/mode_cycle.png').resize((rectangleButtonResizeX,rectangleButtonResizeY), Image.ANTIALIAS))
-eStop_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/e-stop.png').resize((200,200), Image.ANTIALIAS)) 
-arrowUp_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/up_arrow.png').resize((int(arrowResizeX/2),int(arrowResizeY/2)), Image.ANTIALIAS)) 
-arrowDown_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/down_arrow.png').resize((int(arrowResizeX/2),int(arrowResizeY/2)), Image.ANTIALIAS))
-play_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/play_button.png').resize((squareButtonResize,squareButtonResize), Image.ANTIALIAS))
-pause_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/pause_button.png').resize((squareButtonResize,squareButtonResize), Image.ANTIALIAS))
-stop_img = ImageTk.PhotoImage(Image.open('C:/Users/rbiss/Documents/Universite de Sherbrooke/Etudes/Session 4/PCBUS/assets/stop_button.png').resize((squareButtonResize,squareButtonResize), Image.ANTIALIAS))
-=======
 jogXup_img = ImageTk.PhotoImage(Image.open(
     assetsPath + '/up_X_arrow.png').resize((arrowResizeX, arrowResizeY), Image.ANTIALIAS))
 jogXdown_img = ImageTk.PhotoImage(Image.open(
@@ -82,7 +63,6 @@ pause_img = ImageTk.PhotoImage(Image.open(assetsPath + '/pause_button.png').resi
     (squareButtonResize, squareButtonResize), Image.ANTIALIAS))
 stop_img = ImageTk.PhotoImage(Image.open(assetsPath + '/stop_button.png').resize(
     (squareButtonResize, squareButtonResize), Image.ANTIALIAS))
->>>>>>> 023c40e4adc7a6fb112961b153ad772ed6cd59ac
 
 bdwith = 0
 fontPrincipal = ("Arial", 24)
@@ -111,13 +91,15 @@ incrementX_ddm.config(bd=5, font=fontSecondaire, width=12)
 incrementY_ddm = OptionMenu(root, incrementY_var, *incrementXY_list)
 incrementY_ddm.config(bd=5, font=fontSecondaire, width=12)
 incrementZ_ddm = OptionMenu(root, incrementZ_var, *incrementZ_list)
-<<<<<<< HEAD
 incrementZ_ddm.config(bd=5, font=fontSecondaire, width=12)
 
-incrementX_lbl = Label(root,text="Incréments X :", font=fontPrincipal, width=12, anchor='w')
-incrementY_lbl = Label(root,text="Incréments Y :", font=fontPrincipal, width=12, anchor='w')
-incrementZ_lbl = Label(root,text="Incréments Z :", font=fontPrincipal, width=12, anchor='w')
-machineStatus_lbl = Label(root,text="Machine Status :", font=fontPrincipal)
+incrementX_lbl = Label(root, text="Incréments X :",
+                       font=fontPrincipal, width=12, anchor='w')
+incrementY_lbl = Label(root, text="Incréments Y :",
+                       font=fontPrincipal, width=12, anchor='w')
+incrementZ_lbl = Label(root, text="Incréments Z :",
+                       font=fontPrincipal, width=12, anchor='w')
+machineStatus_lbl = Label(root, text="Machine Status :", font=fontPrincipal)
 X_lbl = Label(root, text="X", font=fontPrincipal)
 Y_lbl = Label(root, text="Y", font=fontPrincipal)
 Z_lbl = Label(root, text="Z", font=fontPrincipal)
@@ -128,22 +110,6 @@ feed_lbl = Label(root, text="Feed =", font=fontSecondaire)
 spindle_lbl = Label(root, text="Spindle =", font=fontSecondaire)
 overwrite_lbl = Label(root, text="Overwrite =", font=fontSecondaire)
 overwrite1_lbl = Label(root, text="Overwrite :", font=fontPrincipal)
-=======
-
-incrementX_lbl = Label(root, text="Incréments X :",
-                       font=font, width=12, anchor='w')
-incrementY_lbl = Label(root, text="Incréments Y :",
-                       font=font, width=12, anchor='w')
-incrementZ_lbl = Label(root, text="Incréments Z :",
-                       font=font, width=12, anchor='w')
-machineStatus_lbl = Label(root, text="Machine Status",
-                          font=font, width=12, anchor='w')
-X_lbl = Label(root, text="X", font=font)
-Y_lbl = Label(root, text="Y", font=font)
-Z_lbl = Label(root, text="Z", font=font)
-position_lbl = Label(root, text="Position", font=font)
-distanceToGo_lbl = Label(root, text="Distance to Go", font=font)
->>>>>>> 023c40e4adc7a6fb112961b153ad772ed6cd59ac
 
 jogXup_btn.place(x=120, y=0)
 jogXdown_btn.place(x=120, y=210)
@@ -184,7 +150,7 @@ overwrite1_lbl.place(x=700, y=300)
 arrowUp_btn.place(x=750, y=350)
 arrowDown_btn.place(x=750, y=415)
 
-eStop_btn.place(x=900,y=525)
+eStop_btn.place(x=900, y=525)
 
 
 root.mainloop()
