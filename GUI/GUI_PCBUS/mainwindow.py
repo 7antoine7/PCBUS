41335761c55c9f4ec79a4f1a09997f803be2f26d
rbiss2626 <bisr0901@usherbrooke.ca>
2022-03-05 00:25:22 -0500

    Ajout slider increments XYZ,  commande homing et mesh, tentative de tableau pour le status (a revoir)

--- GUI/GUI_PCBUS/mainwindow.py
+++ GUI/GUI_PCBUS/mainwindow.py
@@ -1,6 +1,7 @@
 import sys
 
 from PySide6 import QtGui, QtWidgets
+from PySide6.QtWidgets import *
 
 from form import Ui_MainWindow
 import resources
@@ -14,6 +15,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.gcodeView.setReadOnly(True)
 
+        self.creatingTableStatus()
+
+        #Il faudrait trouver une meilleure facon, car pour l'instant le Z
+        #va de 0 a 10 et puis je ne sais pas comment mettre les unites.
+        self.IncXSld.valueChanged.connect(self.valueIncX.setNum)
+        self.IncYSld.valueChanged.connect(self.valueIncY.setNum)
+        self.IncZSld.valueChanged.connect(self.valueIncZ.setNum)
+
         #Assignation des bouttons aux fonctions
         self.X_up.clicked.connect(self.moveXUp)
         self.X_down.clicked.connect(self.moveXDown)
@@ -21,36 +30,57 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.Y_down.clicked.connect(self.moveYDown)
         self.Z_up.clicked.connect(self.moveZUp)
         self.Z_down.clicked.connect(self.moveZDown)
+        self.home.clicked.connect(self.homeXY)
+        self.mesh.clicked.connect(self.meshZ)
+
+    #pt pas la meilleure solution, modifiable quand la fenetre run.
+    def creatingTableStatus(self):
+        self.tableStatus.setRowCount(4)
+        self.tableStatus.setColumnCount(3)
+
+        self.tableStatus.setItem(0, 1, QTableWidgetItem("Position"))
+        self.tableStatus.setItem(0, 2, QTableWidgetItem("Distance to Go"))
+        self.tableStatus.setItem(1, 0, QTableWidgetItem("X"))
+        self.tableStatus.setItem(2, 0, QTableWidgetItem("Y"))
+        self.tableStatus.setItem(3, 0, QTableWidgetItem("Z"))
+
+        self.tableStatus.showGrid()
 
     def moveXUp(self):
         #La distance viendrait des toolButton pour choisir les increments
-        distance = "10"
+        distance = str(self.IncXSld.value())
         self.sendCommand("G1 X" + distance)
 
     def moveXDown(self):
         #La distance viendrait des toolButton pour choisir les increments
-        distance = "-10"
+        distance = str(-1*self.IncXSld.value())
         self.sendCommand("G1 X" + distance)
 
     def moveYUp(self):
         #La distance viendrait des toolButton pour choisir les increments
-        distance = "10"
+        distance = str(self.IncYSld.value())
         self.sendCommand("G1 Y" + distance)
 
     def moveYDown(self):
         #La distance viendrait des toolButton pour choisir les increments
-        distance = "-10"
+        distance = str(-1*self.IncYSld.value())
         self.sendCommand("G1 Y" + distance)
 
     def moveZUp(self):
         #La distance viendrait des toolButton pour choisir les increments
-        distance = "10"
+        distance = str(1*self.IncZSld.value())
         self.sendCommand("G1 Z" + distance)
 
     def moveZDown(self):
         #La distance viendrait des toolButton pour choisir les increments
-        distance = "-10"
+        distance = str(-1*self.IncZSld.value())
         self.sendCommand("G1 Z" + distance)
+
+    def homeXY(self):
+        self.sendCommand("G28 X0 Y0")
+
+    def meshZ(self):
+        self.sendCommand("G30")
 
     def sendCommand(self, command):
         self.gcodeView.appendPlainText(command)
